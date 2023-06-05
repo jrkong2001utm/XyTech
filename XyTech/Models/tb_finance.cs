bc66aef9a75a383e6007c12747ed0633197a0519
Zhi Yan <zhiyanyong01@gmail.com>
2023-06-06 00:30:39 +0800

    update model

--- XyTech/Models/tb_finance.cs
+++ XyTech/Models/tb_finance.cs
@@ -11,8 +11,7 @@ namespace XyTech.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class tb_finance
     {
         public int f_id { get; set; }
@@ -22,8 +21,7 @@ namespace XyTech.Models
         public string f_desc { get; set; }
         public string f_transactiontype { get; set; }
         public string f_paymentmethod { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime? f_date { get; set; }
+        public System.DateTime f_date { get; set; }
         public string f_receipt { get; set; }
     
         public virtual tb_user tb_user { get; set; }
