3de8a8dff0c1e088b5601f4851c617c547a438b5
Zhi Yan <zhiyanyong01@gmail.com>
2023-06-12 23:16:08 +0800

    dashboard - room

--- XyTech/Controllers/HomeController.cs
+++ XyTech/Controllers/HomeController.cs
@@ -6,6 +6,10 @@ using System.Web;
 using System.Web.Mvc;
 using XyTech.Attributes;
 using XyTech.Models;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Net;
 
 namespace XyTech.Controllers
 {
@@ -26,7 +30,12 @@ namespace XyTech.Controllers
             ViewBag.landlord = db.tb_landlord.Where(l => l.l_due <= DateTime.Today && l.l_active == "1").ToList();
             ViewBag.countlandlord = db.tb_landlord.Count(l => l.l_due <= DateTime.Today && l.l_active == "1");
             ViewBag.lenroom = db.tb_room.Count(r => r.r_availability == 1);
-            ViewBag.room = db.tb_room.Where(r => r.r_availability == 1).ToList();
+
+            var tb_room = db.tb_room.Include(t => t.tb_floor).Where(p => p.r_active.Equals("active"));
+            ViewBag.room = tb_room;
+
+
+            //ViewBag.room = db.tb_room.Where(r => r.r_availability == 1).ToList();
             ViewBag.counttenant = db.tb_tenant.Count(t => t.t_outdate <= DateTime.Today && (t.t_paymentstatus == 2 || t.t_paymentstatus == 3));
             ViewBag.lentenant = db.tb_tenant.Count();
             ViewBag.leninvestor = db.tb_investor.Count(i => i.i_active == "active");
